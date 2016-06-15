314daed32c56cabcde2003cd0c2260dc3c112aaa
rahultechracers <rsharma@grepruby.com>
2016-06-15 14:12:53 +0530

    Update tax_calc.rb

--- tax_calc.rb
+++ tax_calc.rb
@@ -4,7 +4,7 @@ require './print.rb'
 filename=gets.chomp
 input_file=Input.new(filename)
 input_list=input_file.tokenize
-costs=Calculate_tax.new(input_list)
+costs=CalculateTax.new(input_list)
 Detailed_tax=costs.tax_calculation
 result=Printer.new(Detailed_tax)
 result.receipt_generate
